ce41e4d0e3a1996dffb8d704add865dc467f1a57
danielschaaf <daniel.schaaf@smoothstack.com>
2021-07-07 13:00:36 -0700

    updating files

--- src/com/ss/ds/jb/three/AppendText.java
+++ src/com/ss/ds/jb/three/AppendText.java
@@ -4,8 +4,8 @@ public class AppendText {
 
 	public static void main(String[] args) {
 		// please change file path as this is for my computer. 
-		AddText.addTextToFile("C:\\Users\\Danie\\git\\AssignmentsForSmoothStack\\resources\\test.txt", "this is a test message");
-		AddText.addTextToFile("C:\\Users\\Danie\\git\\AssignmentsForSmoothStack\\resources\\test.txt", "we are adding this text to the file");
+		AddText.addTextToFile("C:\\Users\\Danie\\git\\SmoothStackEssentials\\resources\\test.txt", "this is a test message");
+		AddText.addTextToFile("C:\\Users\\Danie\\git\\SmoothStackEssentials\\resources\\test.txt", "we are adding this text to the file");
 	}
 
 }
